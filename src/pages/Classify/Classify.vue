c76e4e3948a076dc44c2d46d170619ac50a8771f
xiwang <xiwang@email.com>
2019-03-10 16:02:21 +0800

    'mesite'

---
+++ src/pages/Classify/Classify.vue
@@ -0,0 +1,28 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+    export default {
+        //接受
+        props: {},
+        //组件
+        components: {},
+
+        data () {
+            return {}
+        },
+        //定义函数
+        methods: {},
+        //计算属性
+        computed: {},
+        //第一次渲染结束
+        mounted (){
+
+        }
+    }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+
+</style>
